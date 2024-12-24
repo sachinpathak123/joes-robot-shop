a34ed0d0503912acb8c3c67bf33dda3bec4139b5
Sachin Pathak <pathaksachin876@gmail.com>
2024-12-24 06:42:16 +0545

    added few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SiteHeaderComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
